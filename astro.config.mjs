e6c646c59e0de25244eb4f97e646bb08312dc865
guybracha <guy.bracha@gmail.com>
2025-09-22 16:34:59 +0300

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://guybracha.github.io', // עדכן אם צריך
+  site: 'https://guybracha.github.io/compass-world-astro/', // הכתובת המלאה של הפרויקט
+  base: '/compass-world-astro/', // תת-נתיב בריפו
   integrations: [react(), mdx(), sitemap()],
   output: 'static',
 });
